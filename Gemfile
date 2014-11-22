4cb8a2372c2144ada80f686705382b317b421cf2
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-11-22 15:18:26 +0200

    Use Postgres database

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
+gem 'pg'
 
 group :development do
   gem 'spring'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141121084666) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "cities", force: true do |t|
     t.string   "name"
     t.integer  "country_id"
