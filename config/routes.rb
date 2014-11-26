c0c568871626ee1872a140c96c1f42a5374ec91b
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-11-26 21:33:33 +0200

    Restrict the routes for the current resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-    resources :countries do
-      resources :cities do
+    resources :countries, only: [:index, :new, :create] do
+      resources :cities, only: [:index, :new, :create] do
         resources :sight_seeings
       end
     end
