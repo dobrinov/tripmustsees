0d9ad3a5ab517b16c78891af04aa9f53c8df2117
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-11-29 23:27:47 +0200

    Raise ActiveRecord::RecordNotFound if no SightSeeing match

--- app/controllers/sight_seeings_controller.rb
+++ app/controllers/sight_seeings_controller.rb
@@ -5,6 +5,10 @@ class SightSeeingsController < ApplicationController
                                .where(             slug: params[:sight_seeing_slug]  )
                                .where(cities:    { slug: params[:city_slug]         })
                                .where(countries: { slug: params[:country_slug]      }).first
+
+    if @sight_seeing.nil?
+      raise ActiveRecord::RecordNotFound
+    end
   end
 
 end
--- spec/controllers/sight_seeings_controller_spec.rb
+++ spec/controllers/sight_seeings_controller_spec.rb
@@ -3,15 +3,30 @@ require "rails_helper"
 RSpec.describe SightSeeingsController, :type => :controller do
 
   describe 'GET show' do
-    it "assigns @sight_seeing" do
-      sight_seeing = create(:sight_seeing)
-      get :show, {
-                    country_slug:      sight_seeing.city.country.slug,
-                    city_slug:         sight_seeing.city.slug,
-                    sight_seeing_slug: sight_seeing.slug
-                 }
+    context "when SightSeeing exist" do
+      it "assigns @sight_seeing" do
+        sight_seeing = create(:sight_seeing)
 
-      expect(assigns(:sight_seeing)).to eq(sight_seeing)
+        get :show, {
+                      country_slug:      sight_seeing.city.country.slug,
+                      city_slug:         sight_seeing.city.slug,
+                      sight_seeing_slug: sight_seeing.slug
+                   }
+
+        expect(assigns(:sight_seeing)).to eq(sight_seeing)
+      end
+    end
+
+    context "when SightSeeing doesn't exist" do
+      it "raises ActiveRecord::RecordNotFound" do
+        expect do
+          get :show, {
+                        country_slug:      'non_exiting',
+                        city_slug:         'non_exiting',
+                        sight_seeing_slug: 'non_exiting'
+                     }
+        end.to raise_error(ActiveRecord::RecordNotFound)
+      end
     end
   end
 end
