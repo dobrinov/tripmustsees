235bd7cab687df9748f112e9a3a8e1258b2b1e93
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-11-29 11:54:04 +0200

    Restructure the  array of the sight seeing cateory seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,14 @@ puts "Creating SightSeeingCategories ..."
 if SightSeeingCategory.any?
   puts "SightSeeingCategories already exist. Skipping..."
 else
-  sight_seeing_categories = %w{Museum Gallery Landmark Palace}
+  sight_seeing_categories = [
+    'Museum',
+    'Gallery',
+    'Landmark',
+    'Palace',
+    'Cathedral'
+  ]
+
   sight_seeing_categories.each { |sc| SightSeeingCategory.create! }
   puts "#{sight_seeing_categories.count} SightSeeingCategories added to the database."
 end
