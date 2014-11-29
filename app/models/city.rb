c9b1911f246be19cc7790e63885f6220b11acb35
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2014-11-29 15:26:56 +0200

    Make the city slug unique in the country scope

--- app/models/city.rb
+++ app/models/city.rb
@@ -6,7 +6,9 @@ class City < ActiveRecord::Base
 
   # Validations
   validates :name, presence: true
-  validates :slug, presence: true, uniqueness: true, format: { with: /[a-z_]+/ }
+  validates :slug, presence: true,
+                   uniqueness: { scope: :country },
+                   format: { with: /[a-z_]+/ }
   validates :latitude, presence: true
   validates :longitude, presence: true
   validates :default_zoom_level, presence: true
