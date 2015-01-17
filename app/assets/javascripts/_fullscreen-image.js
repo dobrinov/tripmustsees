1af15a5ea5d0e420ab36ea80dda31cf81d35c901
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2015-01-17 14:39:10 +0200

    Implement fullsecreen image for the landing page

---
+++ app/assets/javascripts/_fullscreen-image.js
@@ -0,0 +1,66 @@
+/*!
+ * Map jQuery plugin
+ * Author: Deyan Dobrinov
+ */
+
+;(function ($, window, document, undefined){
+
+  var pluginName = 'fullscreenImage',
+      defaults = {
+        selectors: { // BEM
+          container: '.fullscreen-image'
+        }
+      };
+
+  function FullscreenImage(element, options){
+    var self = this;
+
+    self.element = element;
+
+    self.options = $.extend({}, defaults, options) ;
+
+    self._defaults = defaults;
+    self._name = pluginName;
+
+    self.init();
+  }
+
+  FullscreenImage.prototype.init = function(){
+    var self = this;
+
+    self.node = $(self.options.selectors.container);
+
+    $(window).resize(function(){
+      self.resize();
+    })
+
+    self.resize();
+  }
+
+  FullscreenImage.prototype.resize = function(){
+    var self = this;
+    self.node.height(self.getVisibleHeight());
+  }
+
+  FullscreenImage.prototype.getVisibleWidth = function(){
+    return window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth||0;
+  }
+
+  FullscreenImage.prototype.getVisibleHeight = function(){
+    return window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight||0;
+  }
+
+  $.fn[pluginName] = function(options){
+    return this.each(function(){
+      if (!$.data(this, 'plugin_' + pluginName)) {
+        $.data(this, 'plugin_' + pluginName, new FullscreenImage(this, options));
+      }
+    });
+  };
+
+})(jQuery, window, document);
+
+
+$(document).ready(function(){
+  $('.fullscreen-image').fullscreenImage();
+});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,4 +3,5 @@
 //= require jquery.turbolinks
 //= require turbolinks
 
+//= require _fullscreen-image
 //= require _map
