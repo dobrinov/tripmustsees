0dfe05d76cffa803d13c207eb65938667bcdf872
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2015-01-25 22:40:16 +0200

    Password secure admin area

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,7 @@
 module Admin
   class BaseController < ::ApplicationController
+    if Rails.env.production?
+      http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASS']
+    end
   end
 end
