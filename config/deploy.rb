9683c7e5da96387d49abc66513ca680c7f4e80c6
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2015-01-31 19:26:23 +0200

    Remove broken task

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,17 +25,3 @@ set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bundle public/upl
 
 set :format, :pretty
 set :log_level, :debug
-
-namespace :deploy do
-  %w[start stop restart].each do |command|
-    desc "#{command} unicorn server"
-    task command, roles: :app, except: {no_release: true} do
-      run "/etc/init.d/unicorn_#{application} #{command}"
-    end
-  end
-
-  task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-  end
-end
