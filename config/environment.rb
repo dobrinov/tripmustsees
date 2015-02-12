66849e3bb87d3d39f948a1db41c50fe361856773
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2015-02-12 10:50:59 +0200

    Add configuration file

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# Load application configuration
+APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")[Rails.env]
+
 # Initialize the Rails application.
 Rails.application.initialize!
