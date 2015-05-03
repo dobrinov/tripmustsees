dcbe34b96edb7101d6523c9c4554973f754fb63b
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2015-05-04 01:01:34 +0300

    Read production FB credentials from environment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,9 +1,16 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-  provider :facebook, 614311015336881, 'e65cd8a9f297a570148c038e31cfad1f',
-    # client_options: {
-    #   site: 'https://graph.facebook.com/v2.3',
-    #   authorize_url: "https://www.facebook.com/v2.3/dialog/oauth"
-    # },
-    scope: 'email', display: 'page'
+
+  if Rails.env.production?
+    facebook_key    = ENV['FACEBOOK_KEY']
+    facebook_secret = ENV['FACEBOOK_SECRET']
+  else
+    facebook_key    = 614311015336881
+    facebook_secret = 'e65cd8a9f297a570148c038e31cfad1f'
+  end
+
+  provider :facebook, facebook_key, facebook_secret, scope: 'email', display: 'page'
+  # client_options: {
+  #   site: 'https://graph.facebook.com/v2.3',
+  #   authorize_url: "https://www.facebook.com/v2.3/dialog/oauth"
+  # }
 end
